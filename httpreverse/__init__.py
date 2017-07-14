c19f4491c3bae8054288d8751e850d1c3fc44dff
Petri Savolainen <petri.savolainen@koodaamo.fi>
2017-07-14 09:30:24 +0300

    update version

--- httpreverse/__init__.py
+++ httpreverse/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Petri Savolainen"""
 __email__ = 'petri@koodaamo.fi'
-__version__ = '0.1.0'
+__version__ = '0.4.1'
 
 from .httpreverse import expand_jinja, apply_template, parametrize, marshal
 from .httpreverse import marshal_request_params, marshal_request_body
