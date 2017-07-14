2611f33057f9849990048f618bfe6f0cbd9495e9
Petri Savolainen <petri.savolainen@koodaamo.fi>
2017-07-14 09:36:23 +0300

    cleanup

--- httpreverse/__init__.py
+++ httpreverse/__init__.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 
-__author__ = """Petri Savolainen"""
-__email__ = 'petri@koodaamo.fi'
-__version__ = '0.4.1'
-
 from .httpreverse import expand_jinja, apply_template, parametrize, marshal
 from .httpreverse import marshal_request_params, marshal_request_body
 from .httpreverse import _load_parser, _load_generator
