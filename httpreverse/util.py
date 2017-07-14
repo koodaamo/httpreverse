40580570e65885f59390869b88c7dfad8c990560
Petri Savolainen <petri.savolainen@koodaamo.fi>
2017-07-14 08:46:42 +0300

    utility method for preparing an opspec

--- httpreverse/util.py
+++ httpreverse/util.py
@@ -1,5 +1,9 @@
 import json
+from collections import ChainMap
 from xml.etree import ElementTree
+
+from .client import AsyncioAPIClient
+from .httpreverse import apply_template, parametrize, marshal
 
 
 def parse_json(response):
@@ -7,3 +11,21 @@ def parse_json(response):
 
 def parse_xml(response):
    return ElementTree.fromstring(response)
+
+def prepare_opspec(opspec, api, params=None):
+   "process an individual operation spec"
+
+   # apply operation template
+   if "template" in opspec:
+      opspec = apply_template(opspec, api.get("templates", {}))
+
+   # parametrize request
+   context = api.get("contexts", {})
+   if params:
+      context.update(params)
+   parametrize(opspec, context=context)
+
+   # marshal parameters and body
+   marshal(opspec, api.get("defaults", {}))
+
+   return opspec
